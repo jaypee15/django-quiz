8bb6391310f19cc9a5f98a8be4339981fa183a57
jaypee15 <okoyejohnpaul15@gmail.com>
2024-03-18 11:18:18 +0100

    ci/cd: add build file and read from env files

--- core/settings.py
+++ core/settings.py
@@ -1,24 +1,24 @@
 import os
+
 from dotenv import load_dotenv
-
-
-load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(px$c_s+61=db&8jw%pt$0#-1!)0mb*a$i_^fm31lrq!!=1m#3"
+SECRET_KEY = os.get_env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.get_env("DEBUG", 0).lower() in ["true", "t", "1"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.get_env("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -111,6 +112,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
